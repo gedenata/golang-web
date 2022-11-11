b27b46399e1e18339ba39c3347b2b5624a28f656
gedenata <igedetirtanata@gmail.com>
2022-11-11 14:52:54 +0800

    chore: using server-mus as handler multiple route

---
+++ serve_mux_test.go
@@ -0,0 +1,27 @@
+package golang_web
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestServeMux(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello-world", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello world")
+	})
+	mux.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi")
+	})
+
+	server := http.Server{
+		Addr:    "localhost:8088",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
