9012941a1ff3f46db3bb2cd1c41bc0a0cea3cb5d
gedenata <igedetirtanata@gmail.com>
2022-11-11 14:52:27 +0800

    chore: add handler request

---
+++ handler_test.go
@@ -0,0 +1,23 @@
+package golang_web
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello world")
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8081",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
